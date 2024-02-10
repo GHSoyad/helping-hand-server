46b970513ea9005ad86bad57c3c00df0c16fe22a
GHSoyad <ghsoyad@gmail.com>
2024-02-10 11:11:18 +0600

    Add categories and donations route

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const app: Express = express();
 app.use(cors());
@@ -16,12 +16,64 @@ async function run() {
   try {
     const database = client.db(process.env.DB_NAME || "");
     const usersCollection = database.collection('users');
-
+    const categoriesCollection = database.collection('categories');
+    const donationsCollection = database.collection('donations');
 
     app.get('/api/v1/users', async (req: Request, res: Response) => {
       const query = {};
       const users = await usersCollection.find(query).toArray();
-      res.send(users);
+      res.status(200).send(users);
+    });
+
+    app.get('/api/v1/categories', async (req: Request, res: Response) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.status(200).send(categories);
+    });
+
+    app.get('/api/v1/donations', async (req: Request, res: Response) => {
+      const searchText = req.query.searchText;
+      const category = req.query.category;
+
+      let query: any = {};
+      if (searchText && category) {
+        query = {
+          $and: [
+            { $or: [{ title: { $regex: searchText, $options: 'i' } }, { description: { $regex: searchText, $options: 'i' } }] },
+            { category: new ObjectId(category as string) } // Convert category string to ObjectId
+          ]
+        };
+      } else if (searchText) {
+        query = {
+          $or: [{ title: { $regex: searchText, $options: 'i' } }, { description: { $regex: searchText, $options: 'i' } }]
+        };
+      } else if (category) {
+        query = { category: new ObjectId(category as string) }; // Convert category string to ObjectId
+      }
+
+      const donations = await donationsCollection.aggregate([
+        { $match: query },
+        {
+          $lookup: {
+            from: 'categories',
+            localField: 'category',
+            foreignField: '_id',
+            as: 'category'
+          }
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'organizer',
+            foreignField: '_id',
+            as: 'organizer'
+          }
+        },
+        { $unwind: '$category' },
+        { $unwind: '$organizer' }
+      ]).toArray();
+
+      res.status(200).send(donations);
     })
 
   }
