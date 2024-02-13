10566bdd86298047145f8bd4366916c4a2e3f933
GHSoyad <ghsoyad@gmail.com>
2024-02-13 23:14:53 +0600

    Add statistics route for user-payment history

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient, ObjectId, PushOperator } from "mongodb";
 import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt';
 
@@ -262,14 +262,12 @@ async function run() {
       const donation = req.body;
 
       try {
-        console.log(id, donation)
-
         const insertDonation = {
           ...donation,
           payerId: ObjectId.createFromHexString(donation?.payerId),
           donationId: ObjectId.createFromHexString(id),
+          createdAt: new Date(),
         };
-        console.log(insertDonation)
 
         const filter = { _id: new ObjectId(id) };
 
@@ -278,7 +276,6 @@ async function run() {
           $inc: { raised: parseInt(donation.amount) }, // Increment the value of 'raised' by 100
           $push: { payments: { $each: [] } } // Ensure 'payments' exists as an array
         };
-        console.log(updateDonation)
         // Update the previous document
         const update = await donationsCollection.updateOne(filter, updateDonation);
 
@@ -287,13 +284,14 @@ async function run() {
           const create = await paymentsCollection.insertOne(insertDonation);
           const newDocumentId = create.insertedId; // Get the ID of the newly inserted document
 
-          // Update the previous document again to push the new document ID to the payments array
-          const updateDonation = {
-            $push: { payments: newDocumentId }
-          };
-          await donationsCollection.updateOne(filter, updateDonation);
+          const updatedDonationAgain = await donationsCollection.findOneAndUpdate(
+            filter,
+            {
+              $push: { payments: newDocumentId } as PushOperator<Document>
+            }
+          );
 
-          res.status(200).send({ message: 'Thank You! Donated successfully!', success: true, content: create });
+          res.status(200).send({ message: 'Thank You! Donated successfully!', success: true, content: { ...create, ...updatedDonationAgain } });
         } else {
           // If the update didn't affect any document (perhaps due to wrong ID), send an appropriate error response
           res.status(404).send({ message: 'Donation not found!', success: false });
@@ -303,6 +301,77 @@ async function run() {
         res.status(500).send('Internal Server Error!');
       }
     })
+
+    app.get('/api/v1/statistics/user-total-donation/:id', verifyJWT, async (req: Request, res: Response) => {
+      const userId = req.params.id;
+
+      try {
+        const totalDonationResult = await paymentsCollection.aggregate([
+          {
+            $group: {
+              _id: null, // Group by all documents
+              totalDonation: { $sum: "$amount" } // Compute sum of 'amount' field for all documents
+            }
+          }
+        ]).toArray();
+
+        const userTotalDonationResult = await paymentsCollection.aggregate([
+          {
+            $match: {
+              payerId: ObjectId.createFromHexString(userId) // Match documents with specified payerId
+            }
+          },
+          {
+            $group: {
+              _id: null, // Group by all matched documents
+              userTotalDonation: { $sum: "$amount" } // Compute sum of 'amount' field for matched documents
+            }
+          }
+        ]).toArray();
+
+        // Extract the computed values from the aggregation results
+        const totalDonation = totalDonationResult.length > 0 ? totalDonationResult[0].totalDonation : 0;
+        const userTotalDonation = userTotalDonationResult.length > 0 ? userTotalDonationResult[0].userTotalDonation : 0;
+
+        res.status(200).json({
+          message: 'Donation data found.', success: true, content: {
+            totalDonation,
+            userTotalDonation
+          }
+        });
+      } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get('/api/v1/statistics/get-last-seven-days', async (req: Request, res: Response) => {
+      try {
+        // Calculate the date 7 days ago
+        const sevenDaysAgo = new Date();
+        sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+        const result = await paymentsCollection.aggregate([
+          {
+            $match: {
+              createdAt: { $gte: sevenDaysAgo } // Filter documents created in the last 7 days
+            }
+          },
+          {
+            $group: {
+              _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } }, // Group by day
+              totalAmount: { $sum: "$amount" } // Compute sum of 'amount' field for each day
+            }
+          },
+          {
+            $sort: { _id: 1 } // Sort by date in ascending order
+          }
+        ]).toArray();
+
+        res.status(200).json({ message: 'Donation data found.', success: true, content: result });
+      } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
   }
   finally { }
